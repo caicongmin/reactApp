434f80bbea4a5923466f92c1dedf1934accff73e
caicongmin <caicongmin@yaojigames.com>
2017-06-23 16:57:26 +0800

    配置 web-dev-server热更新 去除dist版本控制

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 /**
  * Created by ccm on 2017/6/23.
  */
-import _ from "lodash"
+import _ from "lodash";
+import './style.css';
+import Icon from './11.png';
+import Library from './library'
 
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['hello', 'webpack'], ' @@@@@@@@@@@@');
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
+}
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+if (module.hot) {
+    module.hot.accept('./library', function() {
+        console.log('Accepting the updated library module!');
+        Library.log();
+    })
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,61 @@
  * Created by ccm on 2017/6/23.
  */
 var path = require('path');
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+var webpack = require('webpack')
+const config = {
+  devtool: "source-map",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
+const devConfig = () => {
+  return Object.assign({
+    devServer: {
+      historyApiFallback: true,
+      stats: 'errors-only',
+      // host: process.env.HOST,
+      port: 8000,
+      contentBase: "./",
+      hot: true,
+      publicPath: '/',
+      // colors: true
     }
+  }, config)
+}
+module.exports = (env) => {
+  console.log("env", env);
+  // console.log("process", process.env)
+  if (env === "development") {
+    return devConfig();
+  }
+  return config;
+
 };
